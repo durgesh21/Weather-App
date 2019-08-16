4b12560511dd8cd8315624e39891eee80c292d0c
Durgesh Kumar <durgesh.17bcs1179@abes.ac.in>
2019-08-16 06:42:12 +0530

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/ef4b2ad7ca51f1ef796bdcea57937352/'+encodeURIComponent(latitude)+','+encodeURIComponent(longitude);
+
+    request({ url:url, json: true }, (error,response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.code === 400) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast;
